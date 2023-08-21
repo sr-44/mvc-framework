8e9698690227b3c2fdcdfbb3b62eab312ed7ce65
sr-44 <mail.sr.44@gmail.com>
2023-08-22 02:15:59 +0500

    created configs and required autoload

---
+++ src/config/init.php
@@ -0,0 +1,17 @@
+<?php
+
+const DEBUG = true;
+define("ROOT", dirname(__DIR__));
+const WWW = ROOT . '/public_html';
+const APP = ROOT . '/app';
+const CORE = ROOT . '/vendor/framework';
+const HELPERS = ROOT . '/vendor/framework/helpers';
+const CACHE = ROOT . '/tmp/cache';
+const LOGS = ROOT . '/tmp/logs';
+const CONFIG = ROOT . '/config';
+const LAYOUT = 'example';
+const PATH = 'http://mvc.loc';
+const ADMIN = PATH . '/admin';
+const NO_IMAGES = 'uploads/no_image.png';
+
+require_once ROOT . '/vendor/autoload.php';
